2cace1fdaaca96d4f39f967a6f2737c5146b6687
3xela <alexander.neagoe@gmail.com>
2025-06-14 14:08:53 -0700

    add linter stuff

--- build.sh
+++ build.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
-
 HIPCC=hipcc
-INCLUDES="-I src/headers"
-
+INCLUDES="-Isrc/headers -Isrc/tensor"
 # Kernel files
 KERNELS="src/kernels/matrix.hip src/kernels/attention.hip src/kernels/activations.hip src/kernels/tiled_matrix.hip src/kernels/softmax.hip"
-
-# Model files  
+# Model files
 MODELS="src/models/transformer.hip"
-
+# Tensor implementation
+TENSOR="src/tensor/tensor.hip"
 # Main/test files
 MAIN="src/main.hip"
-
 # All sources
-SOURCES="$KERNELS $MODELS $MAIN"
-
+SOURCES="$KERNELS $MODELS $TENSOR $MAIN"
 # Output
 OUTPUT="bin/transformer"
-
 # Build
 mkdir -p bin
 $HIPCC $INCLUDES $SOURCES -o $OUTPUT
